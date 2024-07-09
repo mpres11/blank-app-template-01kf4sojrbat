e2a65ff4c07b489a4758924aeedf713e87d4dd48
mpres11 <52675396+mpres11@users.noreply.github.com>
2024-07-09 22:36:15 +0000

    end of tuesday

--- modules/data_prep.py
+++ modules/data_prep.py
@@ -9,7 +9,11 @@ class DataImport:
         pass
 
 
-    def fetch_and_clean_data():
+    def fetch_and_clean_data(self):
         df = pd.read_excel('national-register-listed-20240115.xlsx', engine='openpyxl')
         return df
+    
 
+    def get_state_names(self):
+        state_names = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+        return state_names
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,31 @@ import pandas as pd
 from modules.data_prep import DataImport
 
 
-st.title("🎈 My new app")
+st.title("⭐️ Worthy of Preservation")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-
+st.title('My title')
+st.subheader('My sub')
 st.button('Hit me')
 
-app_data = DataImport().fetch_and_clean_data
+st.markdown("## 🛠️ What is the TOP Skill for Data Analysts?!?")
+
+#get the dataframe
+app_data = DataImport().fetch_and_clean_data()
+
+#get state names
+states = DataImport().get_state_names()
+
+col1, col2 = st.columns(2)
+
+with col1:
+    col1.write('Column1')
+    select_state = st.selectbox('Select State', states)
+
+with col2:
+    col2.write('Column2')
+
+st.dataframe(data=app_data[:15], width=500, height=500)
+
+st.divider()
